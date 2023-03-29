0b95ecef2121a09b1eaccb5b3f58a3371e76c0c6
CiproGit <76820530+CiproGit@users.noreply.github.com>
2023-03-29 19:07:18 +0200

    Add string and algorithm headers

--- include/utilities.h
+++ include/utilities.h
@@ -2,6 +2,7 @@
 #define UTILITIES_H
 
 #include <vector>
+#include <string>
 
 namespace utilities {
 	std::vector<int> tokenize(std::string input_string); // It parses the input string in integer values
--- src/bar_chart.cpp
+++ src/bar_chart.cpp
@@ -1,5 +1,6 @@
 #include "bar_chart.h"
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
